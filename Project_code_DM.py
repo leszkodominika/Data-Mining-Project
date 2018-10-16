2239bd9e11c7fe7239e717b33f8a98bb8fa9f60f
leszkodominika <43548027+leszkodominika@users.noreply.github.com>
2018-10-17 01:47:49 +0200

    Update Project_code_DM.py

--- Project_code_DM.py
+++ Project_code_DM.py
@@ -4,7 +4,6 @@ Created on Sat Oct 13 23:56:46 2018
 
 @author: dominika.leszko
 """
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -15,6 +14,8 @@ df=pd.read_csv(r'C:\Users\dominika.leszko\Desktop\NOVA IMS\Data Mining\DM Projec
 #from sklearn.preprocessing import Imputer
 df = pd.DataFrame(df)
 pd.set_option('display.max_columns', None)
+
+#DOMINIKA: Why replacing empty columns with NaN?
 #from sklearn.preprocessing import Imputer
 #df = df.where(df!='')  # Replacing empty values with NaN
 #df=df.values
@@ -24,20 +25,71 @@ pd.set_option('display.max_columns', None)
 #df[:,-2:-1] = imputer.transform(df[:,-2:-1])
 
 df.info()
+df.describe()
 
-#Renaming columns
+#Renaming columns for easier analysis
 df.columns.values
 
 coldict={'Customer Identity':'CustId', 'First Policy´s Year':'1stPolYear', 'Brithday Year':'BirthYear',
        'Educational Degree':'EduDegree', 'Gross Monthly Salary':'GrossMthSalary',
        'Geographic Living Area':'GeoLivArea', 'Has Children (Y=1)':'HasChild',
-       'Customer Monetary Value':'CustMonetVal', 'Claims Rate':'ClaimRate', 'Premiums in LOB: Motor':'PremLOB',
+       'Customer Monetary Value':'CustMonetVal', 'Claims Rate':'ClaimRate', 'Premiums in LOB: Motor':'PremLOBMotor',
        'Premiums in LOB: Household':'PremLOBHousehold', 'Premiums in LOB: Health':'PremLOBHealth',
        'Premiums in LOB:  Life':'PremLOBLife', 'Premiums in LOB: Work Compensations':'PremLOBWorkCompensation'}
 
 df.rename(columns=coldict, inplace=True)
 
 df.columns.values
+
+##############################Handling Error Data##############################################################
+#
+#   1st Pol Year drop >2016 DONE
+#   Bday Year drop >2016 and <1900 DONE
+#   no error in education DONE
+#   gross month salary-drop outliers ????????????????????
+#   geography no errors DONE
+#   has children no errors DONE
+#   customer monetary, claims rate lets keep for now
+#   LOB all good  cause from system
+
+df.shape#10296 rows, 14 columns
+
+
+df['1stPolYear'].describe()
+#Drop values >2016, as the database comes from 2016
+df = df.drop(df[df['1stPolYear']>2016].index)
+
+
+df['BirthYear'].describe()
+#Drop values >2016 and <1900
+df=df.drop(df[df['BirthYear']>2016].index)
+df=df.drop(df[df['BirthYear']<1900].index)
+
+df['GrossMthSalary'].describe()
+sns.boxplot(x=df['GrossMthSalary'])
+plt.xlim(30000,)
+df=df.drop(df[df['GrossMthSalary']>30000].index)
+
+#We dropped only 4 rows...
+
+#Can we drop odd LOB Premiums? See boxplots
+
+sns.boxplot(x=df['PremLOBMotor'])
+df=df.drop(df[df['PremLOBMotor']>2000].index)
+
+
+sns.boxplot(x=df['PremLOBHousehold'])#sigma rule? here is more outliers
+
+sns.boxplot(x=df['PremLOBHealth'])
+df=df.drop(df[df['PremLOBHealth']>5000].index)
+
+
+sns.boxplot(x=df['PremLOBLife'])
+
+
+sns.boxplot(x=df['PremLOBWorkCompensation'])#sigma rule? more outliers
+
+
 #############################Handling null values################################################################
 
 sns.heatmap(df.isnull())
@@ -45,112 +97,97 @@ sns.heatmap(df.isnull())
 df.isna().any()
 df.isnull().sum(axis=0)
 
-#PremLOBWorkCompensation nulls replace with median
-df['PremLOBWorkCompensation'].hist(bins=40)
-df['PremLOBWorkCompensation']=df['PremLOBWorkCompensation'].fillna(df['PremLOBWorkCompensation'].median())
-df['PremLOBWorkCompensation'].isnull().any()
 
-#PremLOBLife nulls replace with median
-df['PremLOBLife'].hist(bins=40)
-df['PremLOBLife']=df['PremLOBLife'].fillna(df['PremLOBLife'].median())
-df['PremLOBLife'].isnull().any()
+#Replace Birthday with Regression on Salary
 
-#PremLOBHealth nulls replace with median
-df['PremLOBHealth'].hist(bins=130)
-plt.xlim(0,1000)
-df['PremLOBHealth']=df['PremLOBHealth'].fillna(df['PremLOBHealth'].median())
-df['PremLOBHealth'].isnull().any()
+#Replace Salary with Regression on Bday
 
-#PremLOB nulls replace with mean
-df['PremLOB'].hist(bins=130)
-plt.xlim(0,1000)#normal distribution, replace with mean
-df['PremLOB']=df['PremLOBHealth'].fillna(df['PremLOB'].mean())
-df['PremLOB'].isnull().any()
+#1st Year-NN (30 nulls - mean/median?)
+plt.figure(figsize=(8,6))
+df['1stPolYear'].hist()
 
-#HasChild nulls replace with median
-df['HasChild'].hist(bins=10)
-df['HasChild']=df['HasChild'].fillna(df['HasChild'].median())
-df['HasChild'].isnull().any()
+#Education-NN (17 nulls -mean/median?)
+plt.figure(figsize=(8,6))
+df['EduDegree'].hist()
+
+#Geogrpahy-NN (1 null- missing a lot of other columns, DROP!)
+df=df.dropna(subset=['GeoLivArea'])
+
+#Has children-NN (21 nulls, KNN/replace with 1?) after doing knn if 80% has 1 then its good!
+df['HasChild']=df['HasChild'].fillna(1)
+
+#Customer Monet-NO NULLS
+#Claims Ratio - NO NULLS
+
+df['PremLOBWorkCompensation']=df['PremLOBWorkCompensation'].fillna(0)
+df['PremLOBLife']=df['PremLOBLife'].fillna(0)
+df['PremLOBHealth']=df['PremLOBHealth'].fillna(0)
+df['PremLOBMotor']=df['PremLOBMotor'].fillna(0)
 
 
-#GrossMthSalary nulls replace with
-df['GrossMthSalary'].hist(bins=100)
-plt.xlim(0,7000)#normal distribution, replace with mean
-df['GrossMthSalary']=df['GrossMthSalary'].fillna(df['GrossMthSalary'].mean())
-df['GrossMthSalary'].isnull().any()
 
-#BirthYear, 1stPolYear, GeoLivArea, EduDegree
-df.dropna(subset=['BirthYear'], inplace=True)
-df.dropna(subset=['1stPolYear'], inplace=True)
-df.dropna(subset=['GeoLivArea'], inplace=True)
-df.dropna(subset=['EduDegree'], inplace=True)
+#df.dropna(subset=['1stPolYear'], inplace=True)
+##PremLOBWorkCompensation nulls replace with median
+#df['PremLOBWorkCompensation'].hist(bins=40)
+#df['PremLOBWorkCompensation']=df['PremLOBWorkCompensation'].fillna(df['PremLOBWorkCompensation'].median())
+#df['PremLOBWorkCompensation'].isnull().any()
+#
+##PremLOBLife nulls replace with median
+#df['PremLOBLife'].hist(bins=40)
+#df['PremLOBLife']=df['PremLOBLife'].fillna(df['PremLOBLife'].median())
+#df['PremLOBLife'].isnull().any()
+#
+##PremLOBHealth nulls replace with median
+#df['PremLOBHealth'].hist(bins=130)
+#plt.xlim(0,1000)
+#df['PremLOBHealth']=df['PremLOBHealth'].fillna(df['PremLOBHealth'].median())
+#df['PremLOBHealth'].isnull().any()
+#
+##PremLOB nulls replace with mean
+#df['PremLOB'].hist(bins=130)
+#plt.xlim(0,1000)#normal distribution, replace with mean
+#df['PremLOB']=df['PremLOBHealth'].fillna(df['PremLOB'].mean())
+#df['PremLOB'].isnull().any()
+#
+##HasChild nulls replace with median
+#df['HasChild'].hist(bins=10)
+#df['HasChild']=df['HasChild'].fillna(df['HasChild'].median())
+#df['HasChild'].isnull().any()
+#
+#
+##GrossMthSalary nulls replace with
+#df['GrossMthSalary'].hist(bins=100)
+#plt.xlim(0,7000)#normal distribution, replace with mean
+#df['GrossMthSalary']=df['GrossMthSalary'].fillna(df['GrossMthSalary'].mean())
+#df['GrossMthSalary'].isnull().any()
+#
+##BirthYear, 1stPolYear, GeoLivArea, EduDegree
+#df.dropna(subset=['BirthYear'], inplace=True)
+#
+#df.dropna(subset=['EduDegree'], inplace=True)
 
 ################################TRANSFORMING AND CLEANING DATA############################################################
-##EduDegree is an object. Needs encoding
-
-df.describe()
-df.info()
-
-degree_dummies=pd.get_dummies(df['EduDegree'], drop_first=False)
-df.drop(['EduDegree'], axis=1, inplace=True)
-df=pd.concat([df, degree_dummies], axis=1)
 
 #Drop CustId
 df.drop(['CustId'], axis=1, inplace=True)
 
-df.info()#lost only 40 entries after data cleaning.
+#EduDegree is an object. Convert to ordinal.
+
+df.describe()
+df.info()
+
+#degree_dummies=pd.get_dummies(df['EduDegree'], drop_first=False)
+#df.drop(['EduDegree'], axis=1, inplace=True)
+#df=pd.concat([df, degree_dummies], axis=1)
+
+ord_edu=df['EduDegree'].str.split(' - ', 1, expand=True)
+df['EduDegree']=ord_edu[0]
 
 ######################DETECTING OUTLIERS########################
-
-df.columns.values
-
-sns.boxplot(x=df['1stPolYear'])#outlier>2016
-
-
-sns.boxplot(x=df['BirthYear'])#outlier<1926 means older than 90
-
-
-
-sns.boxplot(x=df['GrossMthSalary'])#Very rich?
-
-
-
-sns.boxplot(x=df['GeoLivArea'])#No outliers
-
-
-
-sns.boxplot(x=df['HasChild'])#No outliers
-
-
-
-sns.boxplot(x=df['CustMonetVal'])#negative customer monetary value?not sure#lf_in case you dont know what this means: contract signed 
-#generates a value for the company (in the normalcase the insurance comapny makes money selling polices). Nervertheless each customer 
-#behavies on an idividual basis - therfore the value of the policy(customer here) can be evaluated on its on bases on what has happend in the past
-#and predictions of the future. You can see that customers who have a high claimsrate/claimsratio are usually valued negativ as the money earned by 
-#the insurance company is less that what the insuracne comapny has to pay to the customer for his valid claims, hence i am pretty sure that CustMonetVal and 
-#claimsratio  are negativly correlated# hahah -0.99 if thats no suprise ;)
-
-
-sns.boxplot(x=df['ClaimRate'])#not sure #lf claims rate is defined by Premium/Cost of claims
-
-
-sns.boxplot(x=df['PremLOB'])
-
-sns.boxplot(x=df['PremLOBHousehold'])
-
-sns.boxplot(x=df['PremLOBHealth'])
-
-
-sns.boxplot(x=df['PremLOBLife'])
-
-
-sns.boxplot(x=df['PremLOBWorkCompensation'])
-
 
 ####heat map 
 sns.set(rc={'figure.figsize':(20,20)})
 sns.heatmap(df.corr(), annot=True)
-
 
 
 ##############Z Score
@@ -184,3 +221,5 @@ df_oZ.shape#Z score filters out 1101 outliers.its 10% of dataset
 
 df_oIQR = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
 df_oIQR.shape#IQR filters out 2816 outliers. its 27%
+
+df.head()
